10b5bd0613f1f978ab0871a0f8ec60092b077d5a
suchenxiaoyu <suchenxiaoyu@gmail.com>
2018-07-31 15:23:40 +0800

    angular cli initial

---
+++ src/app/app.component.ts
@@ -0,0 +1,10 @@
+import { Component } from '@angular/core';
+
+@Component({
+  selector: 'zc-root',
+  templateUrl: './app.component.html',
+  styleUrls: ['./app.component.scss']
+})
+export class AppComponent {
+  title = 'deerlu';
+}
